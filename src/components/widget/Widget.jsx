e29234648b723b40e4a2e1bd73825a1614ec17ea
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-09 22:38:23 +0400

    added new Widget for Home Page

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,12 @@
+import "./widget.scss";
+
+const Widget = () => {
+   return (
+      <div className="widget">
+         <div className="left_widget">Left</div>
+         <div className="right_widget">Right</div>
+      </div>
+   );
+};
+
+export default Widget;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
 const Home = () => {
    return (
       <div className="home">
          <Sidebar />
          <div className="container">
             <Navbar />
-            Home Container
+            <div className="widgets_container">
+               <Widget />
+               <Widget />
+               <Widget />
+               <Widget />
+            </div>
          </div>
       </div>
    );
