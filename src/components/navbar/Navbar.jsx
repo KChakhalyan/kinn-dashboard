44e37599560bbf978e007f993dcf49ae777935b8
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-09 21:49:03 +0400

    styled navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,41 @@
 import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 const Navbar = () => {
    return (
       <div className="navbar">
-         <div className="wrapper">Navbar</div>
+         <div className="wrapper">
+            <div className="search">
+               <input type="text" name="" id="" placeholder="Search..." />
+               <SearchOutlinedIcon className="icon search__icon" />
+            </div>
+            <div className="items">
+               <div className="item">
+                  <LanguageOutlinedIcon className="icon" />
+                  <span>English</span>
+               </div>
+               <div className="item">
+                  <DarkModeOutlinedIcon className="icon" />
+               </div>
+               <div className="item">
+                  <FullscreenExitOutlinedIcon className="icon" />
+               </div>
+               <div className="item">
+                  <NotificationsNoneOutlinedIcon className="icon" />
+               </div>
+               <div className="item">
+                  <ChatBubbleOutlineOutlinedIcon className="icon" />
+               </div>
+               <div className="item">
+                  <ListOutlinedIcon className="icon" />
+               </div>
+            </div>
+         </div>
       </div>
    );
 };
