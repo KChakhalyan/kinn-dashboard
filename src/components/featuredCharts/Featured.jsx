3679935097cc8e544935949a2061a4c444af7caa
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-10 00:28:00 +0400

    added progressbar dependency

--- src/components/featuredCharts/Featured.jsx
+++ src/components/featuredCharts/Featured.jsx
@@ -1,7 +1,18 @@
 import "./featured.scss";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 
 const Featured = () => {
-   return <div className="featured">Featured</div>;
+   return (
+      <div className="featured">
+         <div className="top">
+            <h1 className="title">Total Revenue</h1>
+            <MoreVertOutlinedIcon fontSize="small" />
+         </div>
+         <div className="bottom">
+            <div className="featured-chart"></div>
+         </div>
+      </div>
+   );
 };
 
 export default Featured;
