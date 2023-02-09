c2268ac39354d40b8ff07ba3a0b848bd185a4119
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-10 01:19:18 +0400

    Finished featured chart

--- src/components/featuredCharts/Featured.jsx
+++ src/components/featuredCharts/Featured.jsx
@@ -1,5 +1,7 @@
 import "./featured.scss";
 import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
@@ -14,7 +16,34 @@ const Featured = () => {
             <div className="featured-chart">
                <CircularProgressbar value={70} text={"70%"} strokeWidth={3} />
             </div>
-            <p></p>
+            <p className="chart_title">Total sales made today</p>
+            <p className="amount">$420</p>
+            <p className="desc">
+               Previous transactions processing. Last payments may not be icluded
+            </p>
+            <div className="summary">
+               <div className="summary-item">
+                  <div className="item-title">Target</div>
+                  <div className="item-result positive">
+                     <KeyboardArrowUpOutlinedIcon fontSize="small" />
+                     <div className="result-amount">$12.4k</div>
+                  </div>
+               </div>
+               <div className="summary-item">
+                  <div className="item-title">Last Week</div>
+                  <div className="item-result negative">
+                     <KeyboardArrowDownOutlinedIcon fontSize="small" />
+                     <div className="result-amount">$4.2k</div>
+                  </div>
+               </div>
+               <div className="summary-item">
+                  <div className="item-title">Last Month</div>
+                  <div className="item-result positive">
+                     <KeyboardArrowUpOutlinedIcon fontSize="small" />
+                     <div className="result-amount">$2.4k</div>
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
    );
