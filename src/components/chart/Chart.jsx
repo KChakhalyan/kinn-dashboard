59a3cdf58a048c6a005df086527b143928726de7
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-10 01:49:54 +0400

    Added a chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,66 @@
+import { PureComponent } from "react";
+import {
+   AreaChart,
+   Area,
+   XAxis,
+   YAxis,
+   CartesianGrid,
+   Tooltip,
+   ResponsiveContainer,
+} from "recharts";
 import "./chart.scss";
 
 const Chart = () => {
-   return <div className="chart">Chart</div>;
+   const data = [
+      { name: "January", Total: 1200, avarage: 350 },
+      { name: "February", Total: 1100, avarage: 1350 },
+      { name: "March", Total: 800, avarage: 1550 },
+      { name: "April", Total: 950, avarage: 750 },
+      { name: "May", Total: 3200, avarage: 350 },
+      { name: "June", Total: 400, avarage: 500 },
+   ];
+   return (
+      <div className="chart">
+         <div className="chart-title">{`Last 6 month (Revenue)`}</div>
+         <ResponsiveContainer width="100%" aspect={2 / 1}>
+            <AreaChart
+               width={730}
+               height={250}
+               data={data}
+               margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+               <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                     <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                     <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                  </linearGradient>
+                  <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                     <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                     <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                  </linearGradient>
+               </defs>
+               <XAxis dataKey="name" />
+               <YAxis />
+               <CartesianGrid strokeDasharray="3 3" />
+               <Tooltip />
+               <Area
+                  type="monotone"
+                  dataKey="Total"
+                  stroke="#8884d8"
+                  fillOpacity={1}
+                  fill="url(#colorUv)"
+               />
+               <Area
+                  type="monotone"
+                  dataKey="avarage"
+                  stroke="#82ca9d"
+                  fillOpacity={1}
+                  fill="url(#colorPv)"
+               />
+            </AreaChart>
+         </ResponsiveContainer>
+      </div>
+   );
 };
 
 export default Chart;
