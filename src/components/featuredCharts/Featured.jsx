5d516c8d617f8a8b0a78275113530f60103b27e8
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-10 00:15:39 +0400

    Added new Components.

---
+++ src/components/featuredCharts/Featured.jsx
@@ -0,0 +1,7 @@
+import "./featured.scss";
+
+const Featured = () => {
+   return <div className="featured">Featured</div>;
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featuredCharts/Featured";
+import Chart from "../../components/chart/Chart";
 const Home = () => {
    return (
       <div className="home">
@@ -13,6 +15,10 @@ const Home = () => {
                <Widget type="orders" />
                <Widget type="earnings" />
                <Widget type="balance" />
+            </div>
+            <div className="charts_container">
+               <Featured />
+               <Chart />
             </div>
          </div>
       </div>
