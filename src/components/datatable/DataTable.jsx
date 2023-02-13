26d26814dc0dfdccc13561891054c623a3e41f27
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-13 23:26:50 +0400

    complited users page

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -3,45 +3,30 @@ import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../dataTableSource";
 
 const DataTable = () => {
-   // const columns = [
-   //    { field: "id", headerName: "ID", width: 70 },
-   //    { field: "firstName", headerName: "First name", width: 130 },
-   //    { field: "lastName", headerName: "Last name", width: 130 },
-   //    {
-   //       field: "age",
-   //       headerName: "Age",
-   //       type: "number",
-   //       width: 90,
-   //    },
-   //    {
-   //       field: "fullName",
-   //       headerName: "Full name",
-   //       description: "This column has a value getter and is not sortable.",
-   //       sortable: false,
-   //       width: 160,
-   //       valueGetter: (params) => `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-   //    },
-   // ];
+   const actionColumn = [
+      {
+         field: "action",
+         headerName: "Action",
+         width: 200,
+         renderCell: () => {
+            return (
+               <div className="cellAction">
+                  <div className="viewButton">View</div>
+                  <div className="deleteButton">Delete</div>
+               </div>
+            );
+         },
+      },
+   ];
 
-   // const rows = [
-   //    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-   //    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-   //    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-   //    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-   //    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-   //    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-   //    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-   //    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-   //    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-   // ];
    return (
       <div className="dataTable">
          <div style={{ height: "90vh", width: "100%" }}>
             <DataGrid
                rows={userRows}
-               columns={userColumns}
-               pageSize={5}
-               rowsPerPageOptions={[5]}
+               columns={userColumns.concat(actionColumn)}
+               pageSize={9}
+               rowsPerPageOptions={[9]}
                checkboxSelection
             />
          </div>
@@ -50,3 +35,35 @@ const DataTable = () => {
 };
 
 export default DataTable;
+
+// const columns = [
+//    { field: "id", headerName: "ID", width: 70 },
+//    { field: "firstName", headerName: "First name", width: 130 },
+//    { field: "lastName", headerName: "Last name", width: 130 },
+//    {
+//       field: "age",
+//       headerName: "Age",
+//       type: "number",
+//       width: 90,
+//    },
+//    {
+//       field: "fullName",
+//       headerName: "Full name",
+//       description: "This column has a value getter and is not sortable.",
+//       sortable: false,
+//       width: 160,
+//       valueGetter: (params) => `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+//    },
+// ];
+
+// const rows = [
+//    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+//    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+//    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+//    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+//    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
--- src/dataTableSource.jsx
+++ src/dataTableSource.jsx
@@ -41,24 +41,24 @@ export const userRows = [
       id: 1,
       username: "Snow",
       img: "https://media.gq-magazine.co.uk/photos/62ac38f82da9f5f89888eba9/16:9/w_640,c_limit/jon-snow-series-1200.jpeg",
-      status: "active",
+      status: "Active",
       email: "1snow@gmail.com",
       age: 35,
    },
    {
       id: 2,
       username: "Jamie Lannister",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://imgix.bustle.com/uploads/image/2020/4/10/54bfef8a-28a8-40c3-b175-133500e67f90-screen-shot-2020-04-10-at-52939-pm.png?w=2000&h=1090&fit=crop&crop=faces&auto=format%2Ccompress",
       email: "2snow@gmail.com",
-      status: "passive",
+      status: "Passive",
       age: 42,
    },
    {
       id: 3,
-      username: "Lannister",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      username: "Cersei Lannister",
+      img: "https://pyxis.nymag.com/v1/imgs/9eb/c1a/a838f7bffb25ed90a376864c85ae94c578-14-cersei-lannister.rsquare.w700.jpg",
       email: "3snow@gmail.com",
-      status: "pending",
+      status: "Pending",
       age: 45,
    },
    {
@@ -66,7 +66,7 @@ export const userRows = [
       username: "Stark",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "4snow@gmail.com",
-      status: "active",
+      status: "Active",
       age: 16,
    },
    {
@@ -74,7 +74,7 @@ export const userRows = [
       username: "Targaryen",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "5snow@gmail.com",
-      status: "passive",
+      status: "Passive",
       age: 22,
    },
    {
@@ -82,7 +82,7 @@ export const userRows = [
       username: "Melisandre",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "6snow@gmail.com",
-      status: "active",
+      status: "Active",
       age: 15,
    },
    {
@@ -90,7 +90,7 @@ export const userRows = [
       username: "Clifford",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "7snow@gmail.com",
-      status: "passive",
+      status: "Passive",
       age: 44,
    },
    {
@@ -98,7 +98,7 @@ export const userRows = [
       username: "Frances",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "8snow@gmail.com",
-      status: "active",
+      status: "Active",
       age: 36,
    },
    {
@@ -106,7 +106,7 @@ export const userRows = [
       username: "Roxie",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "snow@gmail.com",
-      status: "pending",
+      status: "Pending",
       age: 65,
    },
    {
@@ -114,7 +114,7 @@ export const userRows = [
       username: "Roxie",
       img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       email: "snow@gmail.com",
-      status: "active",
+      status: "Active",
       age: 65,
    },
 ];
