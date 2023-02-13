45646405ff91657b3bdf5004a342c8060975a739
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-13 23:53:38 +0400

    started single page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 const Single = () => {
-   return <div>Single</div>;
+   return (
+      <div className="single">
+         <Sidebar />
+         <div className="single-container">
+            <Navbar />
+            <div className="top">
+               Top
+               <div className="left">
+                  <h1 className="info-title">User Information</h1>
+                  <div className="item">{/* <img src="" alt="" className="item-img" /> */}</div>
+               </div>
+               <div className="right">Right</div>
+            </div>
+            <div className="bottom">Bottom</div>
+         </div>
+      </div>
+   );
 };
 
 export default Single;
