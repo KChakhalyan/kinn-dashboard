3838c581d293edbb3593428489bae5c8955c3743
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-13 22:42:11 +0400

    mode dummy data

--- src/dataTableSource.js
+++ src/dataTableSource.js
@@ -1,8 +1,46 @@
+export const userColumns = [
+   { field: "id", headerName: "ID", width: 70 },
+   {
+      field: "user",
+      headerName: "User",
+      width: 230,
+      renderCell: (params) => {
+         return (
+            <div className="cellWithImg">
+               <img className="cellImg" src={params.row.img} alt="avatar" />
+               {params.row.username}
+            </div>
+         );
+      },
+   },
+   {
+      field: "email",
+      headerName: "Email",
+      width: 230,
+   },
+
+   {
+      field: "age",
+      headerName: "Age",
+      width: 100,
+   },
+   {
+      field: "status",
+      headerName: "Status",
+      width: 160,
+      renderCell: (params) => {
+         return <div className={`cellWithStatus ${params.row.status}`}>{params.row.status}</div>;
+      },
+   },
+];
+
+// Temporary data
+
 export const userRows = [
    {
       id: 1,
       username: "Snow",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://media.gq-magazine.co.uk/photos/62ac38f82da9f5f89888eba9/16:9/w_640,c_limit/jon-snow-series-1200.jpeg",
       status: "active",
       email: "1snow@gmail.com",
       age: 35,
