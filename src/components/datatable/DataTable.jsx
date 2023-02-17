a1e8f539086ae64e29d494439bf1878d23fc1da1
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-17 22:40:53 +0400

    some adds

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -2,19 +2,28 @@ import "./dataTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../dataTableSource";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const DataTable = () => {
+   const [data, setData] = useState(userRows);
+
+   const handleDelete = (id) => {
+      setData(data.filter((item) => item.id !== id));
+   };
+
    const actionColumn = [
       {
          field: "action",
          headerName: "Action",
          width: 200,
-         renderCell: () => {
+         renderCell: (params) => {
             return (
                <div className="cellAction">
                   <Link to="/users/test" style={{ textDecoration: "none" }}>
                      <div className="viewButton">View</div>
                   </Link>
-                  <div className="deleteButton">Delete</div>
+                  <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>
+                     Delete
+                  </div>
                </div>
             );
          },
@@ -32,7 +41,7 @@ const DataTable = () => {
             </div>
             <DataGrid
                className="dataGrid"
-               rows={userRows}
+               rows={data}
                columns={userColumns.concat(actionColumn)}
                pageSize={9}
                rowsPerPageOptions={[9]}
